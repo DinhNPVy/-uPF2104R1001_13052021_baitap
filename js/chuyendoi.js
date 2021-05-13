5dc0e6f6cbd31cb72668362ad71f5541e5b3ff72
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-05-13 22:49:05 +0700

    upload baitap

--- js/chuyendoi.js
+++ js/chuyendoi.js
@@ -1,6 +1,6 @@
 // Chương trình chuyển đổi từ độ C -> độ F
 //  dinhvy //
-let C = parseInt(prompt("Nhập nhiệt độ cần chuyển từ C snag F: "));
+let C = parseInt(prompt("Nhập nhiệt độ cần chuyển từ C sang F: "));
 
    F = (9*C)/5 + 32;
 
